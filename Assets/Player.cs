9421cf294af72f0f5d941d78abcc5a349dd5dc5d
寜 <54977594+NingInk@users.noreply.github.com>
2021-03-07 14:40:58 +0800

    Done

---
+++ Assets/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    public RandomRoom rr;
+    public GameObject cam;
+    CharacterController cc;
+    [Range(0f, 1f)]
+    public float speed = 1;
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    void Update()
+    {
+        float hh = Input.GetAxis("Horizontal");
+        float vv = Input.GetAxis("Vertical");
+
+        Ray ray = new Ray(transform.position, -Vector3.up);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.GetComponent<Renderer>().enabled)
+            {
+
+                if (!rr.prePos.ContainsKey(hit.collider.transform.position + Vector3.forward))
+                {
+                    if (((hit.collider.transform.position + Vector3.forward).z - transform.position.z) < 0.6f)
+                    {
+                        if (vv > 0)
+                        {
+                            vv = 0;
+                        }
+                    }
+                }
+                if (!rr.prePos.ContainsKey(hit.collider.transform.position - Vector3.forward))
+                {
+                    if ((transform.position.z - (hit.collider.transform.position - Vector3.forward).z) < 0.6f)
+                    {
+                        if (vv < 0)
+                        {
+                            vv = 0;
+                        }
+                    }
+                }
+                if (!rr.prePos.ContainsKey(hit.collider.transform.position + Vector3.right))
+                {
+
+                    if (((hit.collider.transform.position + Vector3.right).x - transform.position.x) < 0.6f)
+                    {
+                        if (hh > 0)
+                        {
+                            hh = 0;
+                        }
+                    }
+
+                }
+
+                if (!rr.prePos.ContainsKey(hit.collider.transform.position - Vector3.right))
+                {
+                    if ((transform.position.x - (hit.collider.transform.position - Vector3.right).x) < 0.6f)
+                    {
+                        if (hh < 0)
+                        {
+                            hh = 0;
+                        }
+                    }
+                }
+
+            }
+        }
+        
+
+        Vector3 move = new Vector3(hh, 0, vv);
+        cc.Move(move * speed);
+    }
+    private void LateUpdate()
+    {
+        cam.transform.position = (transform.position + (Vector3.up * 10));
+    }
+}
--- Assets/RandomRoom.cs
+++ Assets/RandomRoom.cs
@@ -1,72 +1,153 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomRoom : MonoBehaviour
 {
-    public class mydata
-    {
-        public Vector3 pos;
-        public Color col;
-        public List<GameObject> gggg;
-        public mydata(Vector3 pos, Color col, List<GameObject> gggg)
-        {
-            this.pos = pos;
-            this.col = col;
-            this.gggg = gggg;
-        }
-    }
+    public GameObject player;
+    //模型预制体
     public GameObject prefab;
 
-    public int allRoom;
-    public int fightRoom;
+    public int allRoom;//所有房间数量
+    public int fightRoom;//战斗房间数量
 
-    public int fightWay;
+    public int fightWay;//战斗道路数量
 
-    Dictionary<Vector3, GameObject> prePos;
+    public Dictionary<Vector3, GameObject> prePos;//所有模型点位
 
     List<mydata> datas;
 
-    private void Awake()
+    List<GameObject> roomgo;
+    List<GameObject> waygo;
+
+    string[] lines;
+
+    List<List<char>> json;
+
+    int showAR, showFR, showFW;
+
+    private void OnGUI()
     {
+        GUILayout.BeginHorizontal();
+        if (showAR != 0)
+        {
+            GUILayout.TextField($"所有房间：{showAR}");
+        }
+        if (showFR != 0)
+        {
+            GUILayout.TextField($"战斗房间：{showFR}");
+        }
+        if (showFW != 0)
+        {
+            GUILayout.TextField($"战斗道路：{showFW}");
+        }
+        GUILayout.EndHorizontal();
+        if (GUI.Button(new Rect(50, 50, 150, 50), "随机生成地图"))
+        {
+            showAR = 0;
+            showFR = 0;
+            showFW = 0;
+            allRoom = 0;
+            fightRoom = 0;
+            fightWay = 0;
+            RandomGeneration();
+        }
+
+        if (GUI.Button(new Rect(50, 100, 150, 50), "加载地图"))
+        {
+            showAR = 0;
+            showFR = 0;
+            showFW = 0;
+            LoadMap();
+        }
+    }
+
+    private void RandomGeneration()
+    {
+        Destroy(goo);
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+        roomgo = new List<GameObject>();
+        waygo = new List<GameObject>();
         datas = new List<mydata>();
         prePos = new Dictionary<Vector3, GameObject>();
-        allRoom = allRoom == 0 ? Random.Range(3, 10) : allRoom;
+
+        allRoom = allRoom == 0 ? Random.Range(5, 10) : allRoom;
         fightRoom = fightRoom == 0 ? Random.Range(1, allRoom) : fightRoom;
 
         fightWay = fightWay == 0 ? Random.Range(1, 7) : fightWay;
 
-        Debug.Log(allRoom);
-        Debug.Log(fightRoom);
-        Debug.Log(fightWay);
-
+        showAR = allRoom;
+        showFR = fightRoom;
+        showFW = fightWay;
         Create(Vector3.zero, Color.white);
         while (datas.Count > 0)
         {
             if (!Create(datas[0].pos, datas[0].col))
             {
-                foreach (var item in datas[0].gggg)
+                foreach (var item in datas[0].gos)
                 {
+                    waygo.Remove(item);
+                    prePos.Remove(item.transform.position);
                     Destroy(item);
                 }
             }
             datas.RemoveAt(0);
         }
+
+
+        for (int i = 0; i < fightRoom; i++)
+        {
+            do
+            {
+                int index = Random.Range(0, roomgo.Count);
+                if (roomgo[index].GetComponent<Renderer>().material.color == Color.green)
+                {
+                    roomgo[index].GetComponent<Renderer>().material.color = Color.blue;
+                    break;
+                }
+            } while (true);
+        }
+        //foreach (var item in waygo)
+        //{
+        //    Debug.Log(item.GetComponent<Renderer>().material.color == Color.gray);
+        //}
+        for (int i = 0; i < fightWay; i++)
+        {
+            do
+            {
+                int index = Random.Range(0, waygo.Count);
+                if (waygo[index].GetComponent<Renderer>().material.color == Color.gray)
+                {
+                    Debug.Log("set red");
+                    waygo[index].GetComponent<Renderer>().material.color = Color.red;
+                    break;
+                }
+            } while (true);
+        }
+
+        goo = Instantiate(player, new Vector3(0, 1.58f, 0), Quaternion.identity);
+        goo.SetActive(true);
     }
 
     bool Create(Vector3 pos, Color col)
     {
+        if (prePos.ContainsKey(pos) && pos == Vector3.zero)
+            return false;
         if (prePos.ContainsKey(pos))
             return true;
         if (allRoom <= 0)
             return false;
-        GameObject gg = Instantiate(prefab, pos, Quaternion.identity);
-        gg.GetComponent<Renderer>().material.color = col;
+
+        GameObject gg = CreateCube(pos, col);
         prePos.Add(pos, gg);
 
         int len;
         if (col == Color.white)
-            len = 1;
+        { len = 1; }
         else
         {
             len = allRoom > 3 ? Random.Range(2, 4) : Random.Range(1, allRoom + 1);
@@ -76,45 +157,151 @@ public class RandomRoom : MonoBehaviour
 
         for (int i = 0; i < len; i++)
         {
-            Vector3 ddd;
-            do
-            {
-                Vector2 dir;
-                int x = Random.Range(0, 2);
-                if (x == 0)
-                    dir = GetDir(Vector2.up);
-                else
-                    dir = GetDir(Vector2.left);
-                ddd = new Vector3(dir.x, 0, dir.y);
-            } while (prePos.ContainsKey(pos + ddd));
+            Vector3 ddd = GetDir(pos);
 
-            List<GameObject> gggg = new List<GameObject>();
-            for (int j = 0; j < 4; j++)
+            if (ddd == Vector3.zero)
             {
-                GameObject go = Instantiate(prefab, pos + ddd * (j + 1), Quaternion.identity);
-                go.GetComponent<Renderer>().material.color = Color.gray;
-                prePos.Add(pos + ddd * (j + 1), go);
-                gggg.Add(go);
+                return true;
             }
 
-            datas.Add(new mydata(pos + ddd * 5, Color.green, gggg));
+            List<GameObject> gos = new List<GameObject>();
+            for (int j = 0; j < 4; j++)
+            {
+                GameObject go = CreateCube(pos + ddd * (j + 1), Color.gray);
+                prePos.Add(pos + ddd * (j + 1), go);
+                gos.Add(go);
+            }
+
+            datas.Add(new mydata(pos + ddd * 5, Color.green, gos));
         }
         return true;
     }
 
-    Vector2 GetDir(Vector2 v2)
+    GameObject CreateCube(Vector3 pos, Color col)
     {
-        int s;
+        GameObject go = Instantiate(prefab, pos, Quaternion.identity);
+        go.GetComponent<Renderer>().material.color = col;
+        go.transform.SetParent(transform);
 
-        s = Random.Range(0, 2);
-        if (s == 0)
+        if (col == Color.gray)
         {
-            return -v2;
+            waygo.Add(go);
         }
-        else
+        if (col == Color.green)
         {
-            return v2;
+            roomgo.Add(go);
         }
+        if (col == Color.white)
+        {
+            player.transform.position = new Vector3(pos.x, 1.5f, pos.z);
+        }
+        return go;
     }
+
+    Vector3 GetDir(Vector3 pos)
+    {
+        Vector3 ddd;
+        List<Vector2> alldir = new List<Vector2>();
+
+        alldir.Add(Vector2.up);
+        alldir.Add(Vector2.down);
+        alldir.Add(Vector2.left);
+        alldir.Add(Vector2.right);
+
+        do
+        {
+            if (alldir.Count == 0)
+            {
+                return Vector3.zero;
+            }
+            int x = Random.Range(0, alldir.Count);
+            Vector2 dir = alldir[x];
+            alldir.Remove(dir);
+
+            ddd = new Vector3(dir.x, 0, dir.y);
+
+        } while (prePos.ContainsKey(pos + ddd));
+        return ddd;
+    }
+
+    void LoadMap()
+    {
+        Destroy(goo);
+        prePos = new Dictionary<Vector3, GameObject>();
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        json = new List<List<char>>();
+
+        lines = System.IO.File.ReadAllLines(System.Environment.CurrentDirectory + "/Resources/map.txt");
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            json.Add(new List<char>());
+            foreach (var item in lines[i])
+            {
+                json[i].Add(item);
+            }
+        }
+
+
+        System.IO.File.WriteAllText(System.Environment.CurrentDirectory + "/Resources/map.json",
+            JsonConvert.SerializeObject(json));
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                Vector3 pos = new Vector3(i, 0, j);
+                GameObject gg = Instantiate(prefab, pos, Quaternion.identity);
+                prePos.Add(pos, gg);
+                gg.transform.SetParent(transform);
+                switch (lines[i][j])
+                {
+                    case ' ':
+                        gg.GetComponent<Renderer>().enabled = false;
+                        prePos.Remove(pos);
+                        break;
+                    case '0':
+                        gg.GetComponent<Renderer>().material.color = Color.white;
+
+                        break;
+                    case '1':
+                        gg.GetComponent<Renderer>().material.color = Color.gray;
+                        break;
+                    case '2':
+                        gg.GetComponent<Renderer>().material.color = Color.red;
+                        break;
+                    case '3':
+                        gg.GetComponent<Renderer>().material.color = Color.green;
+                        break;
+                    case '4':
+                        gg.GetComponent<Renderer>().material.color = Color.blue;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        goo = Instantiate(player, new Vector3(5, 1.58f, 0), Quaternion.identity);
+        goo.SetActive(true);
+    }
+    GameObject goo;
 }
 
+
+public class mydata
+{
+    public Vector3 pos;
+    public Color col;
+    public List<GameObject> gos;
+    public mydata(Vector3 pos, Color col, List<GameObject> gos)
+    {
+        this.pos = pos;
+        this.col = col;
+        this.gos = gos;
+    }
+}
